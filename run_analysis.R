c2b6f2b342c3ecafbe6e2784d776ffb9d20bf939
Scott Creed <scott.creed@capspire.com>
2019-05-19 21:18:57 -0500

    Fixing format to match requested format for submission.

--- run_analysis.R
+++ run_analysis.R
@@ -89,4 +89,4 @@ avgData <- aggregate(select(finalData,-(c("activityId","activityName","subjectId
 names(avgData) <- paste("avg_",names(avgData),sep="")
 names(avgData)[1:3] <- c("activityId","activityName","subjectId")
 avgData <- arrange(avgData,activityId,subjectId)
-write.csv(avgData,".\\Data\\GettingAndCleaningDataAvgMeanAndStd.csv")
+write.table(avgData,".\\Data\\GettingAndCleaningDataAvgMeanAndStd.csv",row.names = FALSE)
